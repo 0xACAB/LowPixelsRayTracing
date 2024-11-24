600703d0611f671572ebbd7b58bed2462a5f6e6f
mathmeditation <mathmeditation@gmail.com>
2024-11-24 22:21:29 +0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,7 @@ import MobilePage from './mobilePage';
 import React from 'react';
 import isMobileDevice from '@/lib/responsive';
 
-export default function Page() {
-	const mobile: boolean = isMobileDevice(); // execute the function
-	return (
-		<>
-			{
-				mobile ? <MobilePage /> : <DesktopPage />
-			}
-		</>
-	);
+export default async function Page() {
+	const mobile: boolean = await isMobileDevice(); // execute the function
+	return (<>{mobile ? <MobilePage /> : <DesktopPage />}</>);
 }
